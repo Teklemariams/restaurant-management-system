2a7756ed006ab027f5f37efd7b732cf1bbfc3d30
Teklemariam <teklemarry@gmail.com>
2019-05-27 19:44:28 +0300

    Added all basic screen activities

---
+++ app/src/main/java/com/wengel/astenagaj/customer/AddOrderActivity.java
@@ -0,0 +1,15 @@
+package com.wengel.astenagaj.customer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wengel.astenagaj.R;
+
+public class AddOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_order);
+    }
+}
---
+++ app/src/main/java/com/wengel/astenagaj/manager/SalesReportActivity.java
@@ -0,0 +1,15 @@
+package com.wengel.astenagaj.manager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wengel.astenagaj.R;
+
+public class SalesReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sales_report);
+    }
+}
