d1c0a5334aa46697931a533e7f1ec8b1c7bf7ab2
Teklemariam <teklemarry@gmail.com>
2019-06-12 15:56:07 +0300

    Order detail activity layout created

--- app/src/main/java/com/wengel/astenagaj/manager/order_management/OrderDetailActivity.java
+++ app/src/main/java/com/wengel/astenagaj/manager/order_management/OrderDetailActivity.java
@@ -2,14 +2,33 @@ package com.wengel.astenagaj.manager.order_management;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.wengel.astenagaj.R;
 
 public class OrderDetailActivity extends AppCompatActivity {
+    private TextView tableNo;
+    private TextView waiter;
+    private TextView status;
+    private ListView ordersListView;
+    private Button okButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detail);
+        tableNo = findViewById(R.id.orderDetail_tableno_tv);
+        waiter = findViewById(R.id.orderDetail_waiter_tv);
+        status = findViewById(R.id.orderDetail_orderStatus_tv);
+        ordersListView = findViewById(R.id.orderDetail_listView);
+        okButton = findViewById(R.id.orderDetail_okBtn);
+
+
+
+
+
+
     }
 }
